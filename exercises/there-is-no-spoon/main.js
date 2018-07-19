1522f48a985fd5749df20bce4ac9fc990483e021
Christopher <chrisn.code@gmail.com>
2018-07-19 09:43:43 -0600

    morning exercises jokes and spoon done

---
+++ exercises/there-is-no-spoon/main.js
@@ -0,0 +1,61 @@
+var phone = {
+    amount: 1,
+    color: "black",
+    isRinging: false,
+    contacts: ["Barbie", "Jacob", "Dad"]
+}
+var beefJerky = {
+    amount: 1,
+    color1: "red",
+    color2: "black",
+    tastesGood: true,
+    eat: function() {
+        console.log("I'm a " + this.color1 + " and" + this.color2 + " beef jerky stick thats great.");
+    }
+}
+var energyDrink = {
+    amount: 1,
+    color1: "black",
+    color2: "red",
+    isEmpty: false,
+}
+var computerScreen = {
+    amount: 24,
+    color: "black",
+    isOn: true,
+}
+var supportBeam = {
+    amount: 10,
+    color: "white",
+    isSupporting: true,
+}
+var airVentThing = {
+    amount: 1,
+    color: "silver",
+    wire: {
+        name: leroy,
+        age: 1,
+        type: metal,
+        isBroken: fales,
+    }
+}
+var window = {
+    amount: 36,
+    color: "transparent",
+    isOpen: false,
+}
+var plant = {
+    amount: 4,
+    color: "green",
+    isAlive: false,
+}
+var thingBehindDesk = {
+    amount: 1,
+    color: "gold",
+    isWeird: true,
+}
+var mic = {
+    amount: 1,
+    color: "grey",
+    isSpeaking: false,
+}
