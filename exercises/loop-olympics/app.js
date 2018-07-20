90cfd65282901ed2038f4915fb8b11d6e689e59d
Christopher <chrisn.code@gmail.com>
2018-07-19 21:05:29 -0600

    daily nineteenth complete

---
+++ exercises/loop-olympics/app.js
@@ -0,0 +1,20 @@
+//Preliminaries//
+
+for(i = 0; i <= 9; i++){
+    console.log(i)
+};
+
+for(i = 9; i >= 0 ; i--){
+    console.log(i)
+};
+
+var fruit = ["banana", "orange", "apple", "kiwi"];
+
+for(i = 0; i <= 3; i++){
+    console.log(fruit[i])
+};
+
+//Bronze Medal//
+
+
+
