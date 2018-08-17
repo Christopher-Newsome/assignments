87d26c40a8e212a165f49e8c1e331937f9abe82f
Christopher <chrisn.code@gmail.com>
2018-08-17 17:09:39 -0600

    react lesson

---
+++ exercises/my-first-react-app/src/App.js
@@ -0,0 +1,15 @@
+// components are functions thet return JSX
+import React from 'react'
+
+
+const App = () => {
+    return (
+        // can only return one element so has to have a parent
+        <div>
+            <h1>Hello World</h1>
+            <h3>boo</h3>
+        </div>
+    )
+}
+
+export default App
