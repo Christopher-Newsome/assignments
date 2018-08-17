0832515b0c12696ad2351dd9fe6f65526a03ff60
Christopher <chrisn.code@gmail.com>
2018-08-16 23:10:36 -0600

    finished esp practice part 1

--- exercises/axios-todo-practice/app.js
+++ exercises/axios-todo-practice/app.js
@@ -1,10 +1,12 @@
 // var axios = require("axios");
-axios.get("https://api.vschool.io/christopher-newsome/todo").then(function(response){
+function getRequest() {
+    axios.get("https://api.vschool.io/christopher-newsome/todo").then(function(response){
     view(response.data)
     console.dir(response.data)
 }).catch(function(err){
     console.log(err)
 })
+}
 
 function displayData(data){
     // console.dir(data);
@@ -24,13 +26,55 @@ function displayData(data){
 }
 
 function view(data) {
+    document.getElementById("list").innerHTML = "";
     data.forEach(function(todo){
-        var todoItem = document.createElement("div")
+        let todoItem = document.createElement("div")
+
         document.getElementById("list").appendChild(todoItem)
         todoItem.innerHTML =    `
                                 <h3>${todo.title}</h3>
                                 <p>${todo.description}</p>
                                 <p>${todo.price}</p>
+                                <input type = "checkbox">
                                 `
+    let button = document.createElement("button");
+    button.textContent = "DELETE";
+    todoItem.appendChild(button);
+    // button.setAttribute("onclick", `deleting('${todo._id}')`)
+    button.addEventListener("click", function(){
+        axios.delete(`https://api.vschool.io/christopher-newsome/todo/${todo._id}`).then(function(response){
+            console.log(response)
+            getRequest()
+        })
     })
+    })
+
 }
+
+// function deleting(id) {
+//     axios.delete(`https://api.vschool.io/christopher-newsome/todo/${id}`).then(function(response){
+//             console.log(response)
+//             getRequest()
+//     })
+// }
+
+document.postForm.addEventListener("submit", function(event){
+    event.preventDefault();
+    const postObj = {
+        title: document.postForm.title.value,
+        description: document.postForm.description.value,
+        price: document.postForm.price.value
+    }
+    axios.post("https://api.vschool.io/christopher-newsome/todo", postObj).then(function(response){
+        console.log(response.data)
+        getRequest()
+
+    })
+})
+getRequest()
+// axios.delete("https://api.vschool.io/christopher-newsome/todo/<todoid>").then(function(response){
+//     view(response.data)
+//     console.dir(response.data)
+// }).catch(function(err){
+//     console.log(err)
+// })
--- exercises/es6-practice/app.js
+++ exercises/es6-practice/app.js
@@ -30,23 +30,48 @@ console.log(petObjects)
 
 // Be aware that if JavaScript sees a { directly after the => it will think it's starting a function, and not starting an object, so the : will be an unexpected symbol.
 
-// const vegetables = carrots.map(function(carrot){
-//     return {type: "carrot", name: carrot}
-// })
+const carrots = ["bright orange", "ripe", "rotten"]
+
+const vegetables = carrots.map((carrot) => {
+    return {type: "carrot", name: carrot}
+})
+console.log(vegetables)
 // Re-write this .filter() using a fat arrow function:
 
-// friends = people.filter(function(person){
-//     return !!person.friendly
-// })
+const people = [
+    {
+        name: 'Princess Peach',
+        friendly: false
+    },
+    {
+        name: 'Luigi',
+        friendly: true
+    },
+    {
+        name: 'Mario',
+        friendly: true
+    },
+    {
+        name: 'Bowser',
+        friendly: false
+    }
+]
+
+friends = people.filter((person) => {
+    return !!person.friendly
+})
+console.log(friends)
+
 // re-write the following functions to be fat arrow functions:
 
-// function doMathSum(a, b){
-//     return a + b
-// }
+const doMathSum = (a, b) => {
+    return a + b
+}
 
-// var produceProduct = function(a, b){
-//     return a * b
-// }
+const produceProduct = (a, b) => {
+    return a * b
+}
+
 // write a functions that takes a firstName, a lastName, an age and returns a string like the following:
 // Hi Kat Stark, how does it feel to be 40?
 
@@ -56,13 +81,26 @@ console.log(petObjects)
 
 // Use the shorthand syntax to make the following code block take up one line.
 
-// const dogs = animals.filter((animal)=>{
-//   if (animal.type === "dog"){
-//     return true
-//   } else {
-//     return false
-//   }
-// })
+const animals = [
+   {
+       type: "dog",
+       name: "theodore"
+   },
+   {
+       type: "cat",
+       name: "whiskers"
+   },
+   {
+       type: "pig",
+       name: "piglette"
+   },
+   {
+       type: "dog",
+       name: "sparky"
+   }];
+
+const dogs = animals.filter( animal => animal.type === "dog" )
+console.log(dogs)
 // Template Literals
 
 // Write a function that will take `location` and `name` and output this if `location="Hawaii"` and `name="Janice"`: ``` Hi Janice!
